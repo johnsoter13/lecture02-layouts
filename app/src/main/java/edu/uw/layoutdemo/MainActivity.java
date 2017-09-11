30b4d8190822d9e3e4f551366a21b1b615c28b12
Joel Ross <joelross@uw.edu>
2017-09-11 14:29:11 -0700

    Reset for start of lecture

--- app/src/main/java/edu/uw/layoutdemo/MainActivity.java
+++ app/src/main/java/edu/uw/layoutdemo/MainActivity.java
@@ -2,9 +2,6 @@ package edu.uw.layoutdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.ViewStub;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,18 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        setContentView(R.layout.activity_relative);
-//        setContentView(R.layout.input_control_layout);
-
-        Button btn = (Button)findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ViewStub stub = (ViewStub) findViewById(R.id.stub);
-                if(stub != null) {
-                    View inflated = stub.inflate();
-                }
-            }
-        });
     }
 }
