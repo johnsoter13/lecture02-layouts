c864362b9b8766c30ea3395c627d0432a43af92b
johnsoter13 <john.soter@outlook.com>
2018-10-08 15:13:06 -0700

    worked through demo

--- app/src/main/java/edu/uw/layoutdemo/MainActivity.kt
+++ app/src/main/java/edu/uw/layoutdemo/MainActivity.kt
@@ -2,6 +2,8 @@ package edu.uw.layoutdemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +12,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myB = findViewById<Button>(R.id.button)
+        Log.v(TAG, myB.text.toString())
     }
 }
